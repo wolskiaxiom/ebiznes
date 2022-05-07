3d20a5cdba7cfd60458aa700672e5fb12885e5e4
Lukasz Wolski <wolskiaxiom@interia.pl>
2022-05-07 14:29:52 +0200

    refactor home page to include google authentication

--- ebiznes2021/frontend-react/src/components/Home.js
+++ ebiznes2021/frontend-react/src/components/Home.js
@@ -1,8 +1,27 @@
+import React from "react";
+
 const Home = () => {
     return (
-        <div style={{textAlign: "center"}}>
-            <h1>Welcome to our pet shop!</h1>
-            <img src={"img/logo-shop.png"} style={{alignContent:"center"}} alt={"Friendly dogo here!"}/>
+        <div>
+            <div style={{textAlign: "center"}}>
+                <h1>Witam w sklepie ze zwierzakami!</h1>
+                <a href="https://ebiznes-backend.azurewebsites.net/authenticate/google" className="provider google" title="Google">
+                    <img src={"img/google2.png"} alt="Google" width="64px" height="64px"/>
+                </a>
+            </div>
+            <div style={{textAlign: "center", padding: "20px"}}>
+                <a href="https://ebiznes-backend.azurewebsites.net/signOut">
+                    Kliknij aby wylogować
+                </a>
+            </div>
+            <div style={{textAlign: "center", padding: "20px"}}>
+                <a href="https://ebiznes-backend.azurewebsites.net/">
+                    Polacz z zabezpieczonym endpointem
+                </a>
+            </div>
+            <div style={{textAlign: "center"}}>
+                <img src={"img/logo-shop.png"} style={{alignContent: "center"}} alt={"Friendly dogo here!"}/>
+            </div>
         </div>
     )
 };
